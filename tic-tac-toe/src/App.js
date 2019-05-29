28764761d3a6693d4cb669d483d9b5caddc23745
AlmuHusan <38337359+AlmuHusan@users.noreply.github.com>
2019-05-29 15:29:37 -0400

    Finished Tic Tac Toe

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -9,6 +9,71 @@ const COLUMNS=3;
 const ROW_ARR= new Array(ROWS).fill('');
 const COL_ARR = new Array(COLUMNS).fill('');
 const GRID = ROW_ARR.map(x=>COL_ARR.slice());
+const MIN_TO_WIN=3;
+const INITIAL_STATE ={
+  currentValue: 'X',
+  grid: Lodash.cloneDeep(GRID),
+  hasWon: false,
+  hasWonMessage:null,
+};
+
+const diffCols = ({arr,item})=>{
+  const lastItem = arr[arr.length-1];
+  return item.columnIndex-lastItem.columnIndex;
+}
+const diffRows = ({arr,item})=>{
+  const lastItem = arr[arr.length-1];
+  return item.rowIndex-lastItem.rowIndex;
+}
+const compareToRest=({currentItems,gridItems,winString})=>{// vid 3 explanation 38:15
+  const N=[currentItems];
+  const NE=[currentItems];
+  const E=[currentItems];
+  const SE=[currentItems];
+  const S=[currentItems];
+  const SW=[currentItems];
+  const W=[currentItems];
+  const NW=[currentItems];
+
+  const applyDirection=(item) =>{
+    if(diffRows({arr:N,item})===1&&diffCols({arr:N,item})===0){
+      N.push(item);
+    }
+    else if(diffRows({arr:NE,item})===1&&diffCols({arr:NE,item})===1){
+      NE.push(item);
+    }
+    else if(diffRows({arr:E,item})===0&&diffCols({arr:E,item})===1){
+      E.push(item);
+    }
+    else if(diffRows({arr:SE,item})===-1&&diffCols({arr:SE,item})===1){
+      SE.push(item);
+    }
+    else if(diffRows({arr:S,item})===-1&&diffCols({arr:S,item})===0){
+      S.push(item);
+    }
+    else if(diffRows({arr:SW,item})===-1&&diffCols({arr:SW,item})===-1){
+      SW.push(item);
+    }
+    else if(diffRows({arr:W,item})===0&&diffCols({arr:W,item})===-1){
+      W.push(item);
+    }
+    else if(diffRows({arr:NW,item})===1&&diffCols({arr:NW,item})===-1){
+      NW.push(item);
+    }
+    const arrays=[N,NE,E,SE,S,SW,W,NW];
+    const winningArrays=arrays.filter(arr=>arr.length>=winString);
+    return winningArrays.length>0;
+  }
+  let hasWon=false;
+  let i =0;
+
+  while (i<gridItems.length && !hasWon){
+    hasWon=applyDirection(gridItems[i]);
+    i++
+  }
+  return hasWon;
+}
+
 
 const flattenAndFilterArrays=(grid)=>{
   const output= [];
@@ -33,12 +98,19 @@ const mapGridIndexes=({grid,value})=>{// vid 3
   console.log({mappedItems});
 }
 
+const checkWin = ({gridItems,winString})=>{
+  let hasWon = false;//check what lets is
+  let i =0;
+  while (i<gridItems.length&&!hasWon){
+    hasWon=compareToRest({currentItems:gridItems[i],gridItems,winString});
+
+    i++;
+  }
+  return hasWon;
+}
 
 class App extends React.Component {
-  state = {
-    currentValue: 'X',
-    grid: Lodash.cloneDeep(GRID),
-  }
+  state = Lodash.cloneDeep(INITIAL_STATE);
   
 
 handleClick = ({columnIndex,rowIndex})=>{
@@ -46,23 +118,37 @@ handleClick = ({columnIndex,rowIndex})=>{
   const{
     currentValue,
     grid,
+    hasWon:gameOver,//renaming hasWon avoid conflict with console log
   }=this.state;
+  if(!gameOver){
   const nextValue=currentValue==='X'?'O':'X';
   const cloneGrid=Lodash.cloneDeep(grid);
   cloneGrid[rowIndex][columnIndex]=currentValue;
   const gridItems = mapGridIndexes({grid:cloneGrid, value:currentValue});
-  console.log({gridItems});
-  this.setState({currentValue:nextValue,grid:cloneGrid});
+  const hasWon=checkWin({gridItems, winString:MIN_TO_WIN});
+  //console.log({hasWon});
+  this.setState({
+    currentValue:nextValue,
+    grid:cloneGrid,
+    hasWon,
+    hasWonMessage: hasWon? `Player ${currentValue} has won!`:null,
+  });
+}
 }
 
   render(){
     
-    const grid=this.state.grid;
-    console.log(grid);
+    const {grid , hasWonMessage}=this.state;
+    //console.log(grid);
   return (
     <div style={{textAlign:'center'}}>
      <h1>Tic Tac Toe</h1>
      <Board onClick={this.handleClick} rows={grid}/>
+     <button onClick={()=> this.setState(INITIAL_STATE)}>RESET</button>
+     <h2>
+       {hasWonMessage}
+     </h2>
+     
     </div>
   );
 
